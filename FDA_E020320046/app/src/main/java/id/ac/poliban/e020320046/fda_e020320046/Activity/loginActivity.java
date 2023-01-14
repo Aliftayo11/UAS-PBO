c071ec8306e7181a9dc69202058f0e31eac3acbe
Aliftayo11 <istananegaraku11@gmail.com>
2023-01-14 21:23:23 +0700

    UasPbo_E020320046

---
+++ FDA_E020320046/app/src/main/java/id/ac/poliban/e020320046/fda_e020320046/Activity/loginActivity.java
@@ -0,0 +1,4 @@
+package id.ac.poliban.e020320046.fda_e020320046.Activity;
+
+public class loginActivity {
+}
---
+++ FDA_E020320046/app/src/main/java/id/ac/poliban/e020320046/fda_e020320046/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package id.ac.poliban.e020320046.fda_e020320046.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
